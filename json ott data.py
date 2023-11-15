03c43300403880ef7a02362d373930d2939319ec
ravi4401 <149867322+ravi4401@users.noreply.github.com>
2023-11-15 09:56:39 +0530

    Add files via upload

---
+++ json ott data.py
@@ -0,0 +1,16 @@
+import json
+
+file_new = "ottdata.json"
+
+with open(file_new, "r") as panzer:
+    data = json.load(panzer)
+
+for i in data["ott_data"]:
+    if i["do"] == "null":
+       print(f'{i["name"]} .{i["live"]} @gmail.com')
+        
+    elif i["live"] =="null":
+        print(f'{i["name"]}{i["do"]} @gmail.com')
+
+    else:
+       print(f'{i["name"]} {i["do"]} {i["live"]} @gmail.com')
