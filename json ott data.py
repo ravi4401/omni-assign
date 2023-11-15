7e4547b74974004ce801c00b082c5ff4df3a3461
ravi4401 <149867322+ravi4401@users.noreply.github.com>
2023-11-15 12:49:12 +0530

    Add files via upload

--- json ott data.py
+++ json ott data.py
@@ -4,13 +4,20 @@ file_new = "ottdata.json"
 
 with open(file_new, "r") as panzer:
     data = json.load(panzer)
-
+empty={}
 for i in data["ott_data"]:
     if i["do"] == "null":
-       print(f'{i["name"]} .{i["live"]} @gmail.com')
+       mag_1=(f'{i["name"]} .{i["live"]} @gmail.com')
+       empty[mag_1]={"name":i["name"],
+                     "do":i["do"],"live":i["live"]}
         
     elif i["live"] =="null":
-        print(f'{i["name"]}{i["do"]} @gmail.com')
+        mag_2=(f'{i["name"]}{i["do"]} @gmail.com')
+        empty[mag_2]={"name":i["name"],
+                     "do":i["do"],"live":i["live"]}
 
     else:
-       print(f'{i["name"]} {i["do"]} {i["live"]} @gmail.com')
+       mag_3=(f'{i["name"]} {i["do"]} {i["live"]} @gmail.com')
+       empty[mag_3]={"name":i["name"],
+                     "do":i["do"],"live":i["live"]}
+print(empty)
